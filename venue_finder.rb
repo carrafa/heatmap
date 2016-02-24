0679c287e0d5acd0e4c3aa7ab9ffe89f926e8384
Joshua Carrafa <carrafa@gmail.com>
2016-02-24 16:56:52 -0500

    formats, heroku fix

--- venue_finder.rb
+++ venue_finder.rb
@@ -14,19 +14,10 @@ def get_venues(search, ll)
   return venues.to_json
 end
   
-
 def venue_search(search, ll)
   response = HTTParty.get("https://api.foursquare.com/v2/venues/search?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=20130815&ll=#{ll}&query=#{search}&limit=50")
   venues = response['response']['venues']
   return venues
-end
-
-
-def ll_mover(latLng, offset_1, offset_2)
-  ll_array = latLng.split(",").map{|s| s.to_f}
-  ll_array[0] = ll_array[0] + offset_1
-  ll_array[1] = ll_array[1] + offset_2
-  return ll_array.join(",").to_s
 end
 
 def get_lls(latLng, offset)
@@ -37,4 +28,11 @@ def get_lls(latLng, offset)
   lls << ll_mover( latLng, offset * -1, offset * -1 )
   lls << ll_mover( latLng, offset* 0 , offset* 0 )
   return lls
+end
+
+def ll_mover(latLng, offset_1, offset_2)
+  ll_array = latLng.split(",").map{|s| s.to_f}
+  ll_array[0] = ll_array[0] + offset_1
+  ll_array[1] = ll_array[1] + offset_2
+  return ll_array.join(",").to_s
 end
